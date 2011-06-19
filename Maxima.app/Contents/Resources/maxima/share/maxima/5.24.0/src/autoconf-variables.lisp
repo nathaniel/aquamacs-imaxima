85d68ae1fcdcee2dd793c5690cff365d2442adeb
Nathaniel <nathaniel.cunningham@gmail.com>
2011-06-18 22:46:08 -0500

    new versions of maxima (5.24.0) and gnuplot (4.4.3)

---
+++ Maxima.app/Contents/Resources/maxima/share/maxima/5.24.0/src/autoconf-variables.lisp
@@ -0,0 +1,18 @@
+; -*- Lisp -*-
+(in-package :maxima)
+
+(defparameter *autoconf-variables-set* "@variables_set@")
+(defparameter *autoconf-prefix* "/Users/drasko/Desktop/Maxima.app/Contents/Resources/maxima")
+(defparameter *autoconf-exec_prefix* "/Users/drasko/Desktop/Maxima.app/Contents/Resources/maxima")
+(defparameter *autoconf-package* "maxima")
+(defparameter *autoconf-version* "5.24.0")
+(defparameter *autoconf-libdir* "/Users/drasko/Desktop/Maxima.app/Contents/Resources/maxima/lib")
+(defparameter *autoconf-libexecdir* "/Users/drasko/Desktop/Maxima.app/Contents/Resources/maxima/libexec")
+(defparameter *autoconf-datadir* "/Users/drasko/Desktop/Maxima.app/Contents/Resources/maxima/share")
+(defparameter *autoconf-infodir* "/Users/drasko/Desktop/Maxima.app/Contents/Resources/maxima/share/info")
+(defparameter *autoconf-host* "i686-apple-darwin10.7.0")
+(defparameter *autoconf-win32* "false")
+(defparameter *autoconf-ld-flags* "")
+ 
+(defparameter *maxima-source-root* "/Users/drasko/Projects/maxima-5.24.0")
+(defparameter *maxima-default-layout-autotools* "true")
--- Maxima.app/Contents/Resources/share/maxima/5.21.1/src/sloop.lisp
+++
@@ -1,5 +0,0 @@
-(in-package :cl-sloop)
-(defmacro sloop (&rest body)
-  (warn "Using deprecated macro 'sloop'. Use 'loop' instead.")
-  `(loop ,@body))
-
