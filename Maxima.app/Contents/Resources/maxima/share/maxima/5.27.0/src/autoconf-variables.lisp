cf0b67e5ccc434d855d07c9f331b5027224a1c3f
Nathaniel <nathaniel.cunningham@gmail.com>
2012-08-20 21:13:55 -0500

    Update to latest versions of Maxima.app and maxima source code.

---
+++ Maxima.app/Contents/Resources/maxima/share/maxima/5.27.0/src/autoconf-variables.lisp
@@ -0,0 +1,18 @@
+; -*- Lisp -*-
+(in-package :maxima)
+
+(defparameter *autoconf-variables-set* "@variables_set@")
+(defparameter *autoconf-prefix* "/Users/andrej/Desktop/Maxima.app/Contents/Resources/maxima")
+(defparameter *autoconf-exec_prefix* "/Users/andrej/Desktop/Maxima.app/Contents/Resources/maxima")
+(defparameter *autoconf-package* "maxima")
+(defparameter *autoconf-version* "5.27.0")
+(defparameter *autoconf-libdir* "/Users/andrej/Desktop/Maxima.app/Contents/Resources/maxima/lib")
+(defparameter *autoconf-libexecdir* "/Users/andrej/Desktop/Maxima.app/Contents/Resources/maxima/libexec")
+(defparameter *autoconf-datadir* "/Users/andrej/Desktop/Maxima.app/Contents/Resources/maxima/share")
+(defparameter *autoconf-infodir* "/Users/andrej/Desktop/Maxima.app/Contents/Resources/maxima/info")
+(defparameter *autoconf-host* "i686-apple-darwin11.3.0")
+(defparameter *autoconf-win32* "false")
+(defparameter *autoconf-ld-flags* "")
+ 
+(defparameter *maxima-source-root* "/Users/andrej/Projects/maxima-5.27.0")
+(defparameter *maxima-default-layout-autotools* "true")
